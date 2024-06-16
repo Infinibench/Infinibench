b4e9b27bc81615d6bbf08aa9d2dabe0ce42b813b
vlv-bench <vlvbenchmark@gmail.com>
2024-06-17 01:51:04 +0300

    Add files via upload
    First commit

---
+++ global_apprerance/tvqa/Run_full_pipeline.sh
@@ -0,0 +1,7 @@
+
+python global_appearance.py 
+python collect_together.py
+python remove_empty_folders.py
+
+## Filter the images humanly and after having the unique images, run the following script
+
